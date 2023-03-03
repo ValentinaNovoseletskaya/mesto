77c9a92b93c4c6255ddc0549d8a665d4d83ee0ab
ValentinaNov <novoseletskayav@gmail.com>
2023-03-03 20:46:29 +0300

    add html css js

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let popupElement = document.querySelector(".popup");
+let popupButtonOpen = document.querySelector(".edit-button");
+let popupButtonClose = popupElement.querySelector(".popup__close-button");
+let nameProfile = document.querySelector(".info__name");
+let jobProfile = document.querySelector(".info__job");
+let nameInput = popupElement.querySelector(".popup__name");
+let jobInput = popupElement.querySelector(".popup__job");
+let popupButtonSave = popupElement.querySelector(".popup__save-button");
+let popupForm = popupElement.querySelector(".popup__content");
+
+function handleFormSubmit (event) {
+    event.preventDefault();
+    let nameValue = nameInput.value;
+    nameProfile.textContent = nameValue;
+    let jobValue = jobInput.value;
+    jobProfile.textContent = jobValue;
+    closePopup();
+}
+
+popupForm.addEventListener('submit', handleFormSubmit);
+
+
+let openPopup = function() {
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+    popupElement.classList.add("popup_opened")
+}
+
+let closePopup = function() {
+    popupElement.classList.remove("popup_opened")
+}
+
+popupButtonOpen.addEventListener("click", openPopup);
+popupButtonClose.addEventListener("click", closePopup);
